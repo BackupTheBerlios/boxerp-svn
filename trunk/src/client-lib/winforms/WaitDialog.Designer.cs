b7f6ebf3021898ff298ddd386b7da3de938800d1
jwormy <jwormy@76fb4815-070b-0410-89ce-b98ed835b4be>
2007-08-23 03:48:36 +0000

    added controls to form and organized them out.

--- trunk/src/client-lib/winforms/WaitDialog.Designer.cs
+++ trunk/src/client-lib/winforms/WaitDialog.Designer.cs
@@ -1,38 +1,111 @@
+//
+// Copyright (c) 2007, Boxerp Project (www.boxerp.org)
+//
+// Copyright (C) 2005,2006 Shidix Technologies (www.shidix.com)
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted provided that the following
+// conditions are met:
+// Redistributions of source code must retain the above
+// copyright notice, this list of conditions and the following
+// disclaimer.
+// Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials
+// provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+//
+using System.Windows.Forms;
+using System.ComponentModel;
+
 namespace Boxerp.Client.WindowsForms
 {
-	partial class WaitDialog
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class WaitDialog
+    {
+        private Label infoLabel;
+        private ProgressBar progressBarControl;
+        private Button cancelButton;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
 
-		#region Windows Form Designer generated code
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "WaitDialog";
-		}
+        #region Windows Form Designer generated code
 
-		#endregion
-	}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "WaitDialog";
+
+            this.progressBarControl = new System.Windows.Forms.ProgressBar();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBarControl
+            // 
+            this.progressBarControl.Location = new System.Drawing.Point(12, 35);
+            this.progressBarControl.Name = "progressBarControl";
+            this.progressBarControl.Size = new System.Drawing.Size(268, 23);
+            this.progressBarControl.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBarControl.TabIndex = 0;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Location = new System.Drawing.Point(12, 9);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(268, 23);
+            this.infoLabel.TabIndex = 1;
+            this.infoLabel.Text = "Operation in progress. Please wait...";
+            // 
+            // WaitDialogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 99);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.progressBarControl);
+            this.Name = "WaitDialogForm";
+            this.Text = "Work in progress...";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(WaitDialogForm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WaitDialogForm_FormClosed);
+        }
+
+        #endregion
+    }
 }

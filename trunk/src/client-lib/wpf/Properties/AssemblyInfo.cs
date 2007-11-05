fd98a2a901f28a26b108cdb6da741ab6c3becaed
carlosble <carlosble@76fb4815-070b-0410-89ce-b98ed835b4be>
2007-11-05 12:30:16 +0000

    Working on deployment. It is a hell. I'm putting the msi in the svn as I could need it. Will be remove after the release

--- trunk/src/client-lib/wpf/Properties/AssemblyInfo.cs
+++ trunk/src/client-lib/wpf/Properties/AssemblyInfo.cs
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyKeyFile("boxerp.snk")]
